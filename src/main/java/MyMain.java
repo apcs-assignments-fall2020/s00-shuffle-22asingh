5a0422feffe8e21efacb630abecb0d70d0cf2f92
22asingh <22asingh@athenian.org>
2021-01-11 05:53:49 +0000

    completed both methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,16 +2,48 @@ public class MyMain {
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int index1 = 0;
+        int index2 = arr.length/2;
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i ++){
+            if (i % 2 == 0){
+                arr2[i] = arr[index1];
+                index1 ++;
+            }
+            else {
+                arr2[i] = arr[index2];
+                index2 ++;
+            }
+        }
+        return arr2;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        for (int i = 0; i < arr.length; i ++){
+            int x = arr[i];
+            int newIndex = (int)(Math.random() * arr.length);
+            arr[i] = arr[newIndex];
+            arr[newIndex] = x;
+        }
+        return arr;
     }
 
 
     public static void main(String[] args) {
         // Write some code here to test your methods!
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        int[] arr2 = perfectShuffle(arr);
+        for (int i : arr2){
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        
+        int[] arr3 = selectionShuffle(arr);
+        for (int i : arr3){
+            System.out.print(i + " ");
+        }
     }
 }
